26eeb3bbe200a68825952e55bc0b48162e4a8e4d
ingopastl <ingo.pastl@gmail.com>
2018-07-31 16:48:13 -0300

    O programa também retorna a porcentagem de amino ácidos.

--- main.py
+++ main.py
@@ -28,6 +28,65 @@ def get_cg_percentage(rna):
             cg_count += 1
     return (cg_count/string_len) * 100
 
+def get_amino_frame(rna):
+    amino_count = {"F": 0, "L": 0, "S": 0, "Y": 0, "-": 0, "C": 0, "W": 0, "P": 0, "H": 0, "Q": 0, "R": 0, "I": 0,
+                   "M": 0, "T": 0, "N": 0, "K": 0, "V": 0, "A": 0, "D": 0, "E": 0, "G": 0}
+
+    codon_amino = {"UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+                   "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+                   "UAU": "Y", "UAC": "Y", "UAA": "-", "UAG": "-",
+                   "UGU": "C", "UGC": "C", "UGA": "-", "UGG": "W",
+                   "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+                   "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+                   "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+                   "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+                   "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+                   "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+                   "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+                   "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+                   "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+                   "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+                   "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+                   "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"}
+
+    amino_properties = {"F": ("Aromatic", "HYDROPHOBIC"),
+                        "L": ("Aliphatic", "HYDROPHOBIC"),
+                        "S": ("tiny", "SMALL", "POLAR"),
+                        "Y": ("Aromatic", "HYDROPHOBIC", "POLAR"),
+                        "C": ("HYDROPHOBIC", "SMALL"),
+                        "W": ("Aromatic", "HYDROPHOBIC", "POLAR"),
+                        "P": ("SMALL"),
+                        "H": ("+ve", "Charged", "Aromatic", "HYDROPHONIC", "POLAR"),
+                        "Q": ("POLAR"),
+                        "R": ("+ve", "Charged", "POLAR"),
+                        "I": ("Aliphatic", "HYDROPHOBIC"),
+                        "M": ("HYDROPHOBIC"),
+                        "T": ("HYDROPHOBIC", "SMALL", "POLAR"),
+                        "N": ("SMALL", "POLAR"),
+                        "K": ("+ve", "Charged", "POLAR", "HYDROPHOBIC"),
+                        "V": ("Aliphatic", "HYDROPHOBIC", "SMALL"),
+                        "A": ("tiny", "SMALL", "HYDROPHOBIC"),
+                        "D": ("-ve", "Charged", "POLAR", "SMALL"),
+                        "E": ("-ve", "Charged", "POLAR"),
+                        "G": ("tiny", "SMALL", "HYDROPHOBIC"),
+                        "-": ("Stop")}
+
+    codon = ""
+    for i in range(0, len(rna)):  # Registra a ocorrência de todos os codons na string e armazena no dict 'm'
+        codon += rna[i]
+        if (len(codon) == 3):
+            amino_count[codon_amino[codon]] += 1
+            codon = ""
+
+    frame = []
+    total_c = (len(rna) - (len(rna) % 3)) / 3  # Total de codons na string
+
+    for key in amino_count:
+        amino_count[key] = (amino_count[key] / total_c) * 100
+        frame.append(CodonFrame(key, amino_count[key], amino_properties[key]))
+
+    return frame
+
 
 def get_codon_frame(rna):
     m = {"UUU": 0, "UUC": 0, "UUA": 0, "UUG": 0,
@@ -100,7 +159,6 @@ def get_codon_frame(rna):
 
         frame.append(CodonFrame(key, m[key], amino_properties[codon_amino[key]]))
 
-
     return frame
 
 
@@ -118,36 +176,18 @@ def get_six_codon_frames(rna):
 
     return frames
 
+def get_six_amino_frames(rna):
+    rna_reverse = rna[:: -1]  # Inverte a string de rna
 
-# Não sei se é isso o que o projeto requere
-def get_motifs(rna, motifs_len):
-    motif = ""
-    motif_frame_list = []
-    for i in range(0, len(rna) - (motifs_len - 1)):
-        motif = rna[i:i + motifs_len]
-        flag = 0
+    frames = []
+    frames.append(get_amino_frame(rna))
+    frames.append(get_amino_frame(rna[1:]))
+    frames.append(get_amino_frame(rna[2:]))
+    frames.append(get_amino_frame(rna_reverse))
+    frames.append(get_amino_frame(rna_reverse[1:]))
+    frames.append(get_amino_frame(rna_reverse[2:]))
 
-        for object in motif_frame_list:
-            if (object.motif == motif):
-                object.initial_indexes.append(i)
-                flag = 1
-
-        if (flag == 0):
-            motif_frame_list.append(MotifFrame(motif, i))
-
-    file = io.open("motifs.txt", "w")
-    for object in motif_frame_list:
-        file.write(str(object.motif) + str(object.initial_indexes) + "\n")
-
-    return motif_frame_list
-
-
-# Não sei se é isso o que o projeto requere
-def get_motifs_by_length(rna, min_len, max_len):
-    motif_list = []
-    for i in range(min_len, max_len + 1):
-        motif_list += get_motifs(rna, i)
-    return motif_list
+    return frames
 
 
 def main():
@@ -160,10 +200,13 @@ def main():
 
     get_cg_percentage(rna)
 
-    get_six_codon_frames(rna)
+    codon_frames = get_six_codon_frames(rna)
+    amino_frames = get_six_amino_frames(rna)
 
-    # motifs = get_motifs_by_length(rna, 3, 12)
-
+    for i in range(0, len(amino_frames)):
+        print("Frame " + str(i + 1))
+        for j in range(0, len(amino_frames[i])):
+            print(amino_frames[i][j])
 
 if __name__ == "__main__":
     main()
